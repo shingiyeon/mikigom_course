cb1f50c2fc96d217c0029e8b920a44aa0853053f
mikigom <sjh14@gist.ac.kr>
2017-01-16 03:28:12 +0900

    updated

--- Coursera_MEAN/AngularJS/Week3and4/app/scripts/controllers.js
+++ Coursera_MEAN/AngularJS/Week3and4/app/scripts/controllers.js
@@ -67,11 +67,12 @@ angular.module('confusionApp')
   };
 }])
 
-.controller('DishDetailController', ['$scope', '$routeParams', 'menuFactory', function($scope, $routeParams, menuFactory) {
-
-  var dish= menuFactory.getDish(parseInt($routeParams.id,10));
+.controller('DishDetailController', ['$scope', '$stateParams', 'menuFactory', function($scope, $stateParams, menuFactory) {
+  var dish= menuFactory.getDish(parseInt($stateParams.id,10));
   $scope.dish = dish;
 }])
+
+
 
 .controller('DishCommentController', ['$scope', function($scope) {
   //Step 1: Create a JavaScript object to hold the comment from the form
---
+++ Coursera_Scala FP/Week 3/example/src/main/scala/Hello.scala
@@ -0,0 +1,6 @@
+/**
+  * Created by mikigom on 17. 1. 12.
+  */
+object Hello {
+   def main(args: Array[String]) = println("Hello World!")
+}
